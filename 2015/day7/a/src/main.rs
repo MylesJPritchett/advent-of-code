a52f022f123583d2950b65d197d9f2bb679aed78
Myles <mylesjpritchett@gmail.com>
2022-08-17 16:56:05 +1000

    day 7 progress

--- 2015/day7/a/src/main.rs
+++ 2015/day7/a/src/main.rs
@@ -2,7 +2,7 @@ use std::fs::File;
 use std::io::{BufRead, BufReader};
 use regex::Regex;
 use regex::Captures;
-use std::collection::HashMap;
+use std::collections::HashMap;
 
 fn main() {
     
@@ -11,31 +11,33 @@ fn main() {
 
     let re = Regex::new(r"(?:(^[[:lower:]]+|^\d+) |)(?:([[:lower:]]+|[[:upper:]]+) |)(?:([[:lower:]]+|[[:upper:]]+|\d+|) |)-> ([[:lower:]]+)$").unwrap();
     
-    let mut signals = HashMap::new();
-    let mut chache = HashMap::new();
+    let mut signals: HashMap<String, Captures> = HashMap::new();
+    //let mut cache = HashMap::new();
 
     for line in reader.lines() {
-        let line = line.unwrap();
-        let cap = re.captures(&line).expect(&format!("invalid input: {}", line));
-        signals.insert(cap.get(4).to_string(), cap);
+        
+        let line: String = line.unwrap();
+        let cap: Captures = re.captures(line).expect(&format!("invalid input: {}", line));
+        signals.insert(cap[4].to_string(), cap);
     }
     
 }
 
-fn get_val(signals: &HashMap<String, Captures>, cache: &mut HashMap<String, u16>, s: &String) -> u16 {
-    if cahce.contains_key(s) {
-        return *cache.get(s).unwrap();
-    }
-    let v: Result<u16, _> = s.parse();
-    if v.is_ok(){
-        return v.unwrap();
-    }
-    match cap.get(2).unwrap().as_str() {
-        "AND" => 
-        "OR" =>
-        "LSHIFT" =>
-        "RSHIFT" =>
-        "NOT" =>
-        _ => get_val(signals, cache, s)
-    }
-}
+// fn get_val(signals: &HashMap<String, Captures>, cache: &mut HashMap<String, u16>, s: &String) -> u16 {
+//     if cache.contains_key(s) {
+//         return *cache.get(s).unwrap();
+//     }
+//     let v: Result<u16, _> = s.parse();
+//     if v.is_ok(){
+//         return v.unwrap();
+//     }
+//     let r = match signals.get(s).unwrap().get(2).unwrap() {
+//         "AND" => println!("G1: {} and G3:{} -> G4:{}", signals.get(s).unwrap().get(1).unwrap().to_string(), signals.get(s).unwrap().get(3).unwrap().to_string(), signals.get(s).unwrap().get(4).unwrap().to_string()),
+//         "OR" => println!("G1: {} or G3:{} -> G4:{}", signals.get(s).get(1), signals.get(s).get(3), signals.get(s).get(4)),
+//         "LSHIFT" => println!("G1: {} ls G3:{} -> G4:{}", signals.get(s).get(1), signals.get(s).get(3), signals.get(s).get(4)),
+//         "RSHIFT" => println!("G1: {} rs G3:{} -> G4:{}", signals.get(s).get(1), signals.get(s).get(3), signals.get(s).get(4)),
+//         "NOT" => println!("G1: {} not G3:{} -> G4:{}", signals.get(s).get(1), signals.get(s).get(3), signals.get(s).get(4)),
+//         _ => println!("G1: {} -> G4:{}", signals.get(s).get(1), signals.get(s).get(4)),
+//     };
+//     return 0;
+// }
